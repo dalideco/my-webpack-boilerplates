28f843ff33fb575df96febc1f3c41191f499dd6d
dalideco <dalissj1@gmail.com>
2022-01-17 12:57:49 +0100

    adding express javascript

---
+++ express/normal/webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const {CleanWebpackPlugin}= require('clean-webpack-plugin')
+const nodeExternals = require('webpack-node-externals')
+const WebpackShellPlugin = require('webpack-shell-plugin-next');
+
+const {
+    NODE_ENV = 'development',
+  } = process.env;
+
+console.log(NODE_ENV);
+
+module.exports = {
+    mode: NODE_ENV,
+    target: "node",
+    node: {
+        // Need this when working with express, otherwise the build fails
+        __dirname: false,   // if you don't put this is, __dirname
+        __filename: false,  // and __filename return blank or /
+    },
+    
+    entry : {
+        server: "./src/index.js"
+    },
+    output: {
+        path: path.join(__dirname, 'dist'),
+        publicPath: '/',
+        filename: 'main.js'
+    },
+
+    devServer: {
+        port: 8080
+    },
+
+    plugins: [
+        new CleanWebpackPlugin(),
+        new WebpackShellPlugin({
+            onBuildStart:{
+                scripts: ['echo "===> Starting packing with WEBPACK 5"'],
+                blocking: true,
+                parallel: false
+            },
+            onBuildEnd:{
+                scripts: [(NODE_ENV === 'development')?'npm run run:dev':'echo "in production no execution"'],
+                blocking: false,
+                parallel: true
+            }
+        })
+    ],
+    externals: [nodeExternals()],
+
+    resolve: {
+        extensions: [".js",".ts"]
+    },
+}
